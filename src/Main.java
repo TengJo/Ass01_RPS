e3eadb2036384723e00c304c2c5a1cd60572117a
TengJo <tengjh@mail.uc.edu>
2022-02-20 22:06:36 -0500

    Made it more bulletproof

--- src/Main.java
+++ src/Main.java
@@ -4,57 +4,83 @@ import java.util.Scanner;
 public class Main
 {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         Scanner in = new Scanner(System.in);
         String playerA;
         String playerB;
 
 
         System.out.print("Player A, what is your move? (R,P,S): ");
-            playerA = in.nextLine();
+        playerA = in.nextLine();
 
         System.out.print("Player B, what is your move? (R,P,S): ");
         playerB = in.nextLine();
 
-        if (playerA.equalsIgnoreCase(playerB))
+        if (playerA.equalsIgnoreCase("R") && (playerB.equalsIgnoreCase("R")))
         {
             System.out.println("It was a tie!");
         }
-        else
+        else if (playerA.equalsIgnoreCase("P") && (playerB.equalsIgnoreCase("P")))
         {
-            if (playerA.equalsIgnoreCase("R"))
+            System.out.println("It was a tie!");
+        }
+        else if (playerA.equalsIgnoreCase("S") && (playerB.equalsIgnoreCase("S")))
+        {
+            System.out.println("It was a tie!");
+        }
+        else if (playerA.equalsIgnoreCase("R"))
+        {
             {
                 if (playerB.equalsIgnoreCase("P"))
                 {
                     System.out.println("Player B wins!");
                 }
-                else
-                {
-                    System.out.println("Player A wins!");
-                }
-            }
-            else if (playerA.equalsIgnoreCase("S"))
-                {
-                if (playerB.equalsIgnoreCase("P"))
+                else if (playerB.equalsIgnoreCase("S"))
                 {
                     System.out.println("Player A wins!");
                 }
                 else
                 {
-                    System.out.println("Player B wins!");
-                }
-                }
-            else if (playerA.equalsIgnoreCase("P"))
-            {
-                if (playerB.equalsIgnoreCase("R"))
-                {
-                    System.out.println("Player A wins!");
-                }
-                else
-                {
-                    System.out.println("Player B wins!");
+                    System.out.println("Player B has entered an incorrect value, run it again!");
                 }
             }
         }
+        else if (playerA.equalsIgnoreCase("S"))
+        {
+            if (playerB.equalsIgnoreCase("P"))
+            {
+                System.out.println("Player A wins!");
+            }
+            else if (playerB.equalsIgnoreCase("R"))
+            {
+                System.out.println("Player B wins!");
+            }
+            else
+            {
+                System.out.println("Player B has entered an incorrect value, run it again!");
+            }
+        }
+        else if (playerA.equalsIgnoreCase("P"))
+        {
+            if (playerB.equalsIgnoreCase("R"))
+            {
+                System.out.println("Player A wins!");
+            }
+            else if (playerB.equalsIgnoreCase("S"))
+            {
+                System.out.println("Player B wins!");
+            }
+            else
+            {
+                System.out.println("Player B has entered an incorrect value, run it again!");
+            }
+        }
+        else
+        {
+            System.out.println("Player A has entered an incorrect value.");
+        }
+        System.out.println("Do you want to play again.");
+        // We never learned how to loop so, I thought this was fine for now
     }
 }
